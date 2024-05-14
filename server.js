539823e3670a2085424bb5b9e0b6699e2ff73298
FinnKilga <finn.kilga@student.htldornbirn.at>
2024-05-14 09:21:00 +0200

    setup of service

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// demo
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2/promise');
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
+process.env.TOKEN_SECRET;
+const Ajv = require("ajv")
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("hello")
+})
+
+app.listen(3000);
